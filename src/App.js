637de4c6949c7b7f5e0427b968c429fb325c8c76
Neli Diakonidze <n.diakonidze@c02zg6svlvdl.speedport.ip>
2020-05-12 21:40:53 +0200

    add routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Dashboard from './Dashboard/Dashboard';
+import Map from './Map/Map';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+    <Router>
+      <Route path="/Family" component={Dashboard} />
+      <Route path="/Family-address" component={Map}/>
+    </Router>
   );
 }
 
---
+++ src/Map/Map.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Map = () => {
+    return (
+        <div> 
+            <p>I'm the map component</p>
+        </div>
+    )
+};
+
+
+export default Map;
